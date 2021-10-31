fd91d52bb91b292c1437b9b6bdc42ff713ded293
lilXA1 <72554214+lilXA1@users.noreply.github.com>
2021-10-31 23:06:11 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy95F970djBJoBVDmI1W5pz2m5upoDijE",
-  authDomain: "book-change-7a20c.firebaseapp.com",
-  projectId: "book-change-7a20c",
-  storageBucket: "book-change-7a20c.appspot.com",
-  messagingSenderId: "280127890843",
-  appId: "1:280127890843:web:022ccdad7024cc340e2d3e",
-  measurementId: "G-STT9EJKSRD"
+    apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(
